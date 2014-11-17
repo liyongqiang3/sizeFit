e6145e8f38c20a0beb848703eb4e44604bbe3edc
liyongqiangngng3 <1184954731@qq.com>
2014-11-17 17:19:30 +0800

    add test  my viewController

---
+++ TSizeClass/TSizeClass/stViewController.h
@@ -0,0 +1,13 @@
+//
+//  stViewController.h
+//  TSizeClass
+//
+//  Created by 李永强 on 14/11/17.
+//  Copyright (c) 2014年 tongbaotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface stViewController : UIViewController
+
+@end
